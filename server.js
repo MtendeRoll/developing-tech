006956f30eead666a04136f58445e1821e5a9615
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-07-11 14:57:00 -0400

    created and started models and server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+
+const app = express();
+const hbs = exphbs.create({});
+
+const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
