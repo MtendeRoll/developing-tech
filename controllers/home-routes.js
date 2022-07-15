eb510ae0a78f173349fafa8e68fc9e1d908b3eb4
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-07-14 21:01:35 -0400

    updated GET requests for home-routes as well as the the all-posts and single post hbs

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,57 +6,36 @@ const { Post, User, Comment } = require("../models");
 router.get("/", async (req, res) => {
   try {
     // we need to get all Posts and include the User for each (change lines 8 and 9)
-    const postData = await SomeModel.someSequelizeMethod({
-      include: [SomeOtherModel],
+    const postData = await Post.findAll({
+      attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at",
+        //[sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)'), 'comment_count']
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "username"],
+        },
+      ],
     });
     // serialize the data
     const posts = postData.map((post) => post.get({ plain: true }));
     // we should render all the posts here
-    res.render("hmmmm what view should we render?", { posts });
+    res.render("all-posts", { posts });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// my original get "/"
-// router.get("/", (req, res) => {
-//   Post.findAll({
-//     attributes: ["id", "post_url", "title", "created_at"],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-//         include: {
-//           model: User,
-//           attributes: ["username"],
-//         },
-//       },
-//       {
-//         model: User,
-//         attributes: ["username"],
-//       },
-//     ],
-//   })
-//     .then((dbPostData) => {
-//       const posts = dbPostData.map((post) => post.get({ plain: true }));
-//       // pass a single post object into the homepage template
-//       //res.render("homepage", dbPostData[0]);
-//       //res.render("homepage", dbPostData[0].get({ plain: true }));
-//       res.render("homepage", { posts });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
 // get single post
 
 router.get("/post/:id", async (req, res) => {
   try {
-    // what should we pass here? we need to get some data passed via the request body (something.something.id?)
     // change the model below, but not the findByPk method.
-    const postData = await SomeModel.findByPk("????", {
+    const postData = await Post.findByPk(req.params.id, {
       // helping you out with the include here, no changes necessary
       include: [
         User,
@@ -70,8 +49,7 @@ router.get("/post/:id", async (req, res) => {
     if (postData) {
       // serialize the data
       const post = postData.get({ plain: true });
-      // which view should we render for a single-post?
-      res.render("hmmmm what view should we render?", { post });
+      res.render("single-post", { post });
     } else {
       res.status(404).end();
     }
