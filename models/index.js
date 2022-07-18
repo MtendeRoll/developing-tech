531d6246ad80d673b457ff19e4a2a46f4d42331c
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-07-18 14:55:49 -0400

    updated models

--- models/index.js
+++ models/index.js
@@ -17,6 +17,19 @@ Comment.belongsTo(User, {
   onDelete: "CASCADE",
 });
 
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
 module.exports = {
   User,
   Comment,
