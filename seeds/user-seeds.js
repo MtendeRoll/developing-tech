8f3a34e3c1cbe946bdc009d1ce7d3322c4b12c1d
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-08-21 21:22:18 -0400

    updated user seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,7 +1,24 @@
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
 
-const userdata = [];
+const userdata = [
+  {
+    username: "aziza",
+    password: bcrypt.hashSync("password123", 8),
+  },
+  {
+    username: "bongani",
+    password: bcrypt.hashSync("password213", 8),
+  },
+  {
+    username: "cecile",
+    password: bcrypt.hashSync("password312", 8),
+  },
+  {
+    username: "donnavon",
+    password: bcrypt.hashSync("password321", 8),
+  },
+];
 
 const seedUsers = () => User.bulkCreate(userdata, { individualHooks: true });
 
